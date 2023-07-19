ed5fce79a2112370b09a9953d2c5419c7d1be793
uhohjackieson <jaclynbarbieri@gmail.com>
2023-07-19 17:35:07 -0400

    coffee menu complete

--- index.js
+++ index.js
@@ -1,58 +1,65 @@
-// Prompt 1:
-// Clean the coffee_data file:
+// clean up data
+// will probably be using filter in this one so refresh memory of what that does
+// print an array of all the drinks using object dot(or bracket) notation, functions and map method
+// print array of drinks that price <= $5, make a function to return these drinks with this condition => then map it
+// use the filter method to return drinks priced at even numbers
+// print the sum total for all drinks combined (make an array for prices then use reduce method to add these together)
+// using boolean for true, obtain the seasonal drinks then add "with imported beans" after item name, print each one
 
-// Checklist:
-// a. All prices should be numbers.
-// b. All items should be strings.
-// c. Objects and properties should have commas seperating them.
-// d. Connect the coffee_data.js file to the index.js file.
+const coffeeMenu = require("./coffee_data");
 
-const coffeeMenu = [
-    {
-        name: "cappuccino",
-        price: "8",
-        seasonal: false,
-    },
-    {
-        name: espresso,
-        price: 5,
-        seasonal: false,
-    }
-    {
-        name: "latte",
-        price: 5,
-        seasonal: false,
-    },
-    {
-        name: affogato,
-        price: "9",
-        seasonal: true,
-    }, 
-    {
-        name: "macchiato",
-        price: "6",
-        seasonal: false,
-    }
-    {
-        name: americano,
-        price: 7,
-        seasonal: false,
-    }
-    {
-        name: "iced coffee",
-        price: "6",
-        seasonal: false,
-    },
-    {
-        name: frappe,
-        price: "8",
-        seasonal: false,
-    }
-    {
-        name: "cuban espresso",
-        price: 10,
-        seasonal: true,
-    }
-]
+// code is clean and bug-free :)
+coffeeMenu.forEach((item) => {
+    item.price = Number(item.price);
+})
 
-module.exports = coffeeMenu
+// print an array of all the drinks
+const drinks = (item) => {
+    return item.name;
+}
+const drinksArray = coffeeMenu.map(drinks);
+console.log(drinksArray);
+
+// print array of drinks that price <= $5
+const underFive = (item) => {
+    return item.price <= 5
+};
+const drinksUnderFive = coffeeMenu.filter(underFive);
+console.log(drinksUnderFive);
+
+// print array of drinks priced with even numbers
+const evenPrice = (item) => {
+    return item.price % 2 === 0;
+}
+const drinksEvenPrice = coffeeMenu.filter(evenPrice);
+console.log(drinksEvenPrice);
+
+
+// get sum total if you ordered 1 of every drink
+const drinksCost = (item) => {
+    return item.price;
+}
+const drinksCostArray = coffeeMenu.map(drinksCost);
+const sumTotal = (accumulator, currentValue) => {
+    return accumulator + currentValue;
+}
+const totalCost = drinksCostArray.reduce(sumTotal);
+console.log(totalCost);
+
+// add "with imported beans" after each seasonal drink
+const seasonalDrinks = (item) => {
+    return item.seasonal === true;
+}
+const seasonalDrinksArray = coffeeMenu.filter(seasonalDrinks);
+// console.log(seasonalDrinksArray);
+
+seasonalDrinksArray.forEach((item) => {
+    item.name = item.name + " with imported beans";
+})
+console.log(seasonalDrinksArray);
+
+
+
+// console.table(seasonalDrinksArray);
+
+// if typeof item === "string" then return Number(item)
